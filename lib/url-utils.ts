c656b30788e1abeca187d292938316a2b5fea40b
sfjohnson11 <info@sfjohnsonconsulting.com>
2025-05-06 08:27:15 -0700

    Update url-utils.ts

--- lib/url-utils.ts
+++ lib/url-utils.ts
@@ -3,7 +3,7 @@ export function getFullUrl(path: string): string {
   if (path.startsWith('http')) return path
 
   const baseUrl = 'https://msllqpnxwbugvkpnquwx.supabase.co/storage/v1/object/public'
-  const cleanPath = path.replace(/^\/+/, '').replace(/\/{2,}/g, '/')
+  const cleanPath = path.replace(/^\/+/g, '').replace(/\/+/g, '/')
 
   return `${baseUrl}/${cleanPath}`
 }
