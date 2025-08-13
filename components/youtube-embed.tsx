8e40aae8a34fc9faf8795a89336ca02aecf619c7
sfjohnson11 <info@sfjohnsonconsulting.com>
2025-08-13 16:46:31 -0700

    Update youtube-embed.tsx

--- components/youtube-embed.tsx
+++ components/youtube-embed.tsx
@@ -10,7 +10,9 @@ export default function YouTubeEmbed(props: Props) {
   const src =
     "videoId" in props
       ? `https://www.youtube-nocookie.com/embed/${encodeURIComponent(props.videoId)}?autoplay=1&playsinline=1&rel=0&modestbranding=1&controls=1${muted ? "&mute=1" : ""}`
-      : `https://www.youtube-nocookie.com/embed/live_stream?channel=${encodeURIComponent(props.channelId)}&autoplay=1&playsinline=1&rel=0&modestbranding=1&controls=1${muted ? "&mute=1" : ""}`;
+      : `https://www.youtube-nocookie.com/embed/live_stream?channel=${encodeURIComponent(
+          props.channelId
+        )}&autoplay=1&playsinline=1&rel=0&modestbranding=1&controls=1${muted ? "&mute=1" : ""}`;
 
   return (
     <div className="relative w-full" style={{ paddingTop: "56.25%" }}>
