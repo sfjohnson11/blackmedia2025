5f3a470f317faaaf6e2e92968a9f027dfc3d305f
sfjohnson11 <info@sfjohnsonconsulting.com>
2026-01-17 17:31:08 -0800

    Refactor RootLayout function parameter syntax

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,11 +6,7 @@ export const metadata = {
   description: "Black Truth TV Platform",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
